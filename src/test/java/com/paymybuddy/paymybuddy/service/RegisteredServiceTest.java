c28ebb2036b2aa2177b2ced6cf120d34b3bd8f74
OlivierMOREL <oliviermorel.oc1@gmail.com>
2022-11-20 14:53:25 +0100

    feat : TDD RegisterdService.getAllNotAddBy

--- src/test/java/com/paymybuddy/paymybuddy/service/RegisteredServiceTest.java
+++ src/test/java/com/paymybuddy/paymybuddy/service/RegisteredServiceTest.java
@@ -83,7 +83,7 @@ public class RegisteredServiceTest {
 		private Registered registered;
 		
 		@BeforeAll
-		public void setUpForAllCreateATransactionTests() {
+		public void setUpForAllTests() {
 			requestMock = new MockHttpServletRequest();
 			requestMock.setServerName("http://localhost:8080");
 			requestMock.setRequestURI("/createRegistered");
@@ -91,7 +91,7 @@ public class RegisteredServiceTest {
 		}
 
 		@AfterAll
-		public void unSetForAllCreateATransactionTests() {
+		public void unSetForAllTests() {
 			requestMock = null;
 			request = null;
 		}
@@ -181,7 +181,7 @@ public class RegisteredServiceTest {
 	class GetRegisteredTests {
 		
 		@BeforeAll
-		public void setUpForAllCreateATransactionTests() {
+		public void setUpForAllTests() {
 			requestMock = new MockHttpServletRequest();
 			requestMock.setServerName("http://localhost:8080");
 			requestMock.setRequestURI("/getRegistered?email=aaa@aaa.com");
@@ -189,7 +189,7 @@ public class RegisteredServiceTest {
 		}
 
 		@AfterAll
-		public void unSetForAllCreateATransactionTests() {
+		public void unSetForAllTests() {
 			requestMock = null;
 			request = null;
 		}
@@ -244,7 +244,7 @@ public class RegisteredServiceTest {
 	class UpdateRegisteredTests {
 		
 		@BeforeAll
-		public void setUpForAllCreateATransactionTests() {
+		public void setUpForAllTests() {
 			requestMock = new MockHttpServletRequest();
 			requestMock.setServerName("http://localhost:8080");
 			requestMock.setRequestURI("/updateRegistered");
@@ -252,7 +252,7 @@ public class RegisteredServiceTest {
 		}
 
 		@AfterAll
-		public void unSetForAllCreateATransactionTests() {
+		public void unSetForAllTests() {
 			requestMock = null;
 			request = null;
 		}
@@ -489,62 +489,15 @@ public class RegisteredServiceTest {
 	}
 	
 	@Nested
-	@Tag("removeRegisteredTests")
-	@DisplayName("Tests for method removeRegistered")
-	@TestInstance(Lifecycle.PER_CLASS)
-	class RemoverRegisteredTests {
-
-		@BeforeAll
-		public void setUpForAllCreateATransactionTests() {
-			requestMock = new MockHttpServletRequest();
-			requestMock.setServerName("http://localhost:8080");
-			requestMock.setRequestURI("/removeRegistered?email=aaa@aaa.com");
-			request = new ServletWebRequest(requestMock);
-		}
-
-		@AfterAll
-		public void unSetForAllCreateATransactionTests() {
-			requestMock = null;
-			request = null;
-		}
-		
-		@Test
-		@Tag("RegisteredServiceTest")
-		@DisplayName("test removeRegistered should throw UnexpectedRollbackException on IllegalArgumentException")
-		public void removeRegisteredTestShouldThrowsUnexpectedRollbackExceptionOnIllegalArgumentException() {
-			//GIVEN
-			doThrow(new IllegalArgumentException()).when(registeredRepository).deleteById(anyString());
-			//WHEN
-			//THEN
-			assertThat(assertThrows(UnexpectedRollbackException.class,
-					() -> registeredService.removeRegistered("aaa@aaa.com", request))
-					.getMessage()).isEqualTo("Error while removing your profile");
-		}
-		
-		@Test
-		@Tag("RegisteredServiceTest")
-		@DisplayName("test removeRegistered should throw UnexpectedRollbackException on any RuntimeException")
-		public void removeRegisteredTestShouldThrowsUnexpectedRollbackExceptionOnAnyRuntimeException() {
-			//GIVEN
-			doThrow(new RuntimeException()).when(registeredRepository).deleteById(anyString());
-			//WHEN
-			//THEN
-			assertThat(assertThrows(UnexpectedRollbackException.class,
-					() -> registeredService.removeRegistered("aaa@aaa.com", request))
-					.getMessage()).isEqualTo("Error while removing your profile");
-		}
-	}
-	
-	@Nested
 	@Tag("getRegistrantsTests")
 	@DisplayName("Tests for method getRegistrants")
 	@TestInstance(Lifecycle.PER_CLASS)
-	class GetAllRegisteredTests {
+	class GetRegistrantsTests {
 		
 		private Pageable pageRequest;
 
 		@BeforeAll
-		public void setUpForAllCreateATransactionTests() {
+		public void setUpForAllTests() {
 			pageRequest = PageRequest.of(0, 3);
 			requestMock = new MockHttpServletRequest();
 			requestMock.setServerName("http://localhost:8080");
@@ -553,7 +506,7 @@ public class RegisteredServiceTest {
 		}
 
 		@AfterAll
-		public void unSetForAllCreateATransactionTests() {
+		public void unSetForAllTests() {
 			pageRequest = null;
 			requestMock = null;
 			request = null;
@@ -606,6 +559,128 @@ public class RegisteredServiceTest {
 					() -> registeredService.getRegistrants(pageRequest, request))
 					.getMessage()).isEqualTo("Error while getting Registrants");
 		}
+	}
+	
+	@Nested
+	@Tag("getAllAddByTests")
+	@DisplayName("Tests for method getAllAddBy")
+	@TestInstance(Lifecycle.PER_CLASS)
+	class GetAllAddByTests {
+		
+		private Pageable pageRequest;
 
+		@BeforeAll
+		public void setUpForAllTests() {
+			pageRequest = PageRequest.of(0, 3);
+			requestMock = new MockHttpServletRequest();
+			requestMock.setServerName("http://localhost:8080");
+			requestMock.setRequestURI("/getgetAllAddBy?email=ccc@ccc.com");
+			request = new ServletWebRequest(requestMock);
+		}
+
+		@AfterAll
+		public void unSetForAllTests() {
+			pageRequest = null;
+			requestMock = null;
+			request = null;
+		}
+		
+		@Test
+		@Tag("RegisteredServiceTest")
+		@DisplayName("test getAllAddBy should return page of RegisterdForListDTO")
+		public void removeRegisteredTestShouldReturnPageOfRegisterdForListDTO() {
+			//GIVEN : C add A and B
+			Registered registeredA = new Registered("aaa@aaa.com", "aaaPasswd", "Aaa", "AAA", LocalDate.parse("01/01/1991", DateTimeFormatter.ofPattern("dd/MM/yyyy")), "aaaIban");
+			Registered registeredB = new Registered("bbb@bbb.com", "bbbPasswd", "Bbb", "BBB", LocalDate.parse("02/02/1992", DateTimeFormatter.ofPattern("dd/MM/yyyy")), "bbbIban");
+			RegisteredForListDTO registeredForListDTOA = new RegisteredForListDTO("aaa@aaa.com", "Aaa", "AAA");
+			RegisteredForListDTO registeredForListDTOB = new RegisteredForListDTO("bbb@bbb.com", "Bbb", "BBB");
+			List<RegisteredForListDTO> registrantsDTOExpected = Arrays.asList(registeredForListDTOA, registeredForListDTOB);
+			when(registeredRepository.findAllAddByEmail(anyString(), any(Pageable.class))).thenReturn(new PageImpl<Registered>(Arrays.asList(registeredA, registeredB), pageRequest, 2));
+			when(registeredDTOService.registeredToForListDTO(any(Registered.class))).thenReturn(registeredForListDTOA).thenReturn(registeredForListDTOB);
+			
+			//WHEN			
+			Page<RegisteredForListDTO> pageRegistrantsDTOResult = registeredService.getAllAddBy("ccc@ccc.com", pageRequest, request);
+			
+			///THEN
+			assertThat(pageRegistrantsDTOResult.getContent()).containsExactlyElementsOf(registrantsDTOExpected);
+			assertThat(pageRegistrantsDTOResult.getPageable().getPageSize()).isEqualTo(3);
+		}
+		
+		@Test
+		@Tag("RegisteredServiceTest")
+		@DisplayName("test getRegistrants should throw UnexpectedRollbackException on NullPointerException")
+		public void getRegistrantsTestShouldThrowsUnexpectedRollbackExceptionOnNullPointerException() {
+			//GIVEN
+			when(registeredRepository.findAllAddByEmail(anyString(), any(Pageable.class))).thenThrow(new NullPointerException());
+			//WHEN
+			//THEN
+			assertThat(assertThrows(UnexpectedRollbackException.class,
+					() -> registeredService.getAllAddBy("ccc@ccc.com", pageRequest, request))
+					.getMessage()).isEqualTo("Error while getting connections you added");
+			
+		}
+		
+		@Test
+		@Tag("RegisteredServiceTest")
+		@DisplayName("test getRegistrants should throw UnexpectedRollbackException on any RuntimeException")
+		public void getRegistrantsTestShouldThrowsUnexpectedRollbackExceptionOnAnyRuntimeException() {
+			//GIVEN
+			when(registeredRepository.findAllAddByEmail(anyString(), any(Pageable.class))).thenThrow(new RuntimeException());
+			//WHEN
+			//THEN
+			assertThat(assertThrows(UnexpectedRollbackException.class,
+					() -> registeredService.getAllAddBy("ccc@ccc.com", pageRequest, request))
+					.getMessage()).isEqualTo("Error while getting connections you added");
+		}
+	}
+	
+	
+
+	
+	@Nested
+	@Tag("removeRegisteredTests")
+	@DisplayName("Tests for method removeRegistered")
+	@TestInstance(Lifecycle.PER_CLASS)
+	class RemoveRegisteredTests {
+
+		@BeforeAll
+		public void setUpForAllTests() {
+			requestMock = new MockHttpServletRequest();
+			requestMock.setServerName("http://localhost:8080");
+			requestMock.setRequestURI("/removeRegistered?email=aaa@aaa.com");
+			request = new ServletWebRequest(requestMock);
+		}
+
+		@AfterAll
+		public void unSetForAllTests() {
+			requestMock = null;
+			request = null;
+		}
+		
+		@Test
+		@Tag("RegisteredServiceTest")
+		@DisplayName("test removeRegistered should throw UnexpectedRollbackException on IllegalArgumentException")
+		public void removeRegisteredTestShouldThrowsUnexpectedRollbackExceptionOnIllegalArgumentException() {
+			//GIVEN
+			doThrow(new IllegalArgumentException()).when(registeredRepository).deleteById(anyString());
+			//WHEN
+			//THEN
+			assertThat(assertThrows(UnexpectedRollbackException.class,
+					() -> registeredService.removeRegistered("aaa@aaa.com", request))
+					.getMessage()).isEqualTo("Error while removing your profile");
+		}
+		
+		@Test
+		@Tag("RegisteredServiceTest")
+		@DisplayName("test removeRegistered should throw UnexpectedRollbackException on any RuntimeException")
+		public void removeRegisteredTestShouldThrowsUnexpectedRollbackExceptionOnAnyRuntimeException() {
+			//GIVEN
+			doThrow(new RuntimeException()).when(registeredRepository).deleteById(anyString());
+			//WHEN
+			//THEN
+			assertThat(assertThrows(UnexpectedRollbackException.class,
+					() -> registeredService.removeRegistered("aaa@aaa.com", request))
+					.getMessage()).isEqualTo("Error while removing your profile");
+		}
 	}
 }
